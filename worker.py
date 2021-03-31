b9383fda2b4c5e213071bc690a4b991055e3256d
David Jang <58031020+bbq12340@users.noreply.github.com>
2021-03-31 12:19:27 +0900

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,8 +1,5 @@
 from PySide2.QtCore import QThread, QObject, Signal
 import time
-import pandas as pd
-from selenium.webdriver.support.wait import WebDriverWait
-from scraper import start_driver, search_coupang
 
 
 class Worker(QObject):
@@ -15,17 +12,12 @@ class Worker(QObject):
         self.delay = float(delay)
 
     def run(self):
-        browser = start_driver()
-        wait = WebDriverWait(browser, 10)
-        result = []
         for query in self.query_list:
-            data = search_coupang(browser, wait, query)
-            result.append(data)
+            """
+                do something fancy
+                like scraping somthing by query
+            """
             self.progress.emit(
                 ((self.query_list.index(query)+1)/len(self.query_list)*100))
             time.sleep(self.delay)
-        browser.quit()
-        df = pd.DataFrame(result)
-        df.to_csv(f"result/{self.query_list[0]}.csv",
-                  encoding="utf-8-sig", index=False)
         self.finished.emit()
